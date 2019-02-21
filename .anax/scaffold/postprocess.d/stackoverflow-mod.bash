98e4a2bc2d0fc6f6124ef0b0436a381d82e3ecc5
Unsinkablesam <maraphostv@gmail.com>
2019-02-21 21:44:53 +0100

    added postprocess  bash file

---
+++ .anax/scaffold/postprocess.d/stackoverflow-mod.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# anax/remserver
+#
+# Integrate the REM server onto an existing anax installation.
+#
+
+# Copy the configuration files
+rsync -av vendor/anax/stackoverflow-mod/config ./
+
+
+# Copy the documentation
+#rsync -av vendor/anax/remserver/content/index.md ./content/remserver-api.md
