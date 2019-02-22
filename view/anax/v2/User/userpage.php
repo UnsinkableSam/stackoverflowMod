6b3dc8cdceed56912ed49b9d69ffcf09017e14bd
Unsinkablesam <maraphostv@gmail.com>
2019-02-22 21:21:11 +0100

    v1.4 added user views

---
+++ view/anax/v2/User/userpage.php
@@ -0,0 +1,10 @@
+<?php if ($res): ?>
+
+
+  <?php print_r("<h1>" . $res[0]->email . "</h1>" ?? "<br><h3> No logged in </h3>"); ?>
+  <img src="<?php echo $avatar ?>" alt="Avatar">
+  <br>
+  <?php
+
+  print_r($content); ?>
+<?php endif; ?>
